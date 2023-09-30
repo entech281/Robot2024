f5ee7887039747e1d768cf88310cd799dc3e41f7
WhyDoesGodDoThis <101780103+WhyDoesGodDoThis@users.noreply.github.com>
2023-09-30 14:05:38 -0400

    stopping counter carried over

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 36;
-  public static final String GIT_SHA = "69582e20a34f3c463fa8411d201aa1f8f3fae3bb";
-  public static final String GIT_DATE = "2023-09-28 21:01:21 EDT";
-  public static final String GIT_BRANCH = "HEAD";
-  public static final String BUILD_DATE = "2023-09-30 13:46:37 EDT";
-  public static final long BUILD_UNIX_TIME = 1696095997485L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 37;
+  public static final String GIT_SHA = "86c9747bca6fb90e29d482319b8654248074214e";
+  public static final String GIT_DATE = "2023-09-30 13:47:46 EDT";
+  public static final String GIT_BRANCH = "AH_Format_and_testing";
+  public static final String BUILD_DATE = "2023-09-30 13:53:22 EDT";
+  public static final long BUILD_UNIX_TIME = 1696096402607L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
