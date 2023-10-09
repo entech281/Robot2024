6dab8608750c7defed574f8811f305961222d588
eshi <eric.shi.sc@gmail.com>
2023-10-09 19:45:23 -0400

    deleted useless subsystems

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 46;
-  public static final String GIT_SHA = "d1e145b9c06db707f6b013d42ab7d0ca8f8a0368";
-  public static final String GIT_DATE = "2023-10-09 18:59:28 EDT";
+  public static final int GIT_REVISION = 47;
+  public static final String GIT_SHA = "00c99b6235425a3484d84843ae3170407e4248bd";
+  public static final String GIT_DATE = "2023-10-09 19:28:32 EDT";
   public static final String GIT_BRANCH = "EricSSubsystemTransfer_10_9";
-  public static final String BUILD_DATE = "2023-10-09 19:22:41 EDT";
-  public static final long BUILD_UNIX_TIME = 1696893761174L;
+  public static final String BUILD_DATE = "2023-10-09 19:42:57 EDT";
+  public static final long BUILD_UNIX_TIME = 1696894977499L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
