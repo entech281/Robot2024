3e7bd980fb659d568707c5adb6d4ac3e819bc129
John-Molfetner <jtmolfenter07@gmail.com>
2023-10-12 18:25:42 -0400

    merging Eric's changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,15 +5,15 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Robot2024";
+  public static final String MAVEN_NAME = "Robot2024-1";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 47;
-  public static final String GIT_SHA = "00c99b6235425a3484d84843ae3170407e4248bd";
-  public static final String GIT_DATE = "2023-10-09 19:28:32 EDT";
+  public static final int GIT_REVISION = 48;
+  public static final String GIT_SHA = "6dab8608750c7defed574f8811f305961222d588";
+  public static final String GIT_DATE = "2023-10-09 19:45:23 EDT";
   public static final String GIT_BRANCH = "EricSSubsystemTransfer_10_9";
-  public static final String BUILD_DATE = "2023-10-09 19:42:57 EDT";
-  public static final long BUILD_UNIX_TIME = 1696894977499L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2023-10-12 18:24:44 EDT";
+  public static final long BUILD_UNIX_TIME = 1697149484664L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
