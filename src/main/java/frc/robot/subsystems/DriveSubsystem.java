a632778e306f49aaf66d8b7d7441a8c21266da6b
WhyDoesGodDoThis <101780103+WhyDoesGodDoThis@users.noreply.github.com>
2023-11-13 18:28:48 -0500

    might work

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -297,7 +297,6 @@ public class DriveSubsystem extends EntechSubsystem {
             Pose2d pose = getPose().get();
             Pose2d pose2 = new Pose2d(pose.getTranslation(), Rotation2d.fromDegrees(180));
             resetOdometry(pose2);
-            // m_gyro.setAngleAdjustment(180);
         }
     }
 
@@ -379,6 +378,10 @@ public class DriveSubsystem extends EntechSubsystem {
                     RobotConstants.Ports.CAN.REAR_RIGHT_TURNING,
                     RobotConstants.Ports.ANALOG.REAR_RIGHT_TURNING_ABSOLUTE_ENCODER, false);
 
+            // m_gyro.calibrate();
+            // while (m_gyro.isCalibrating()) {
+            // ;
+            // }
             m_gyro = new AHRS(Port.kMXP);
 
             m_gyro.zeroYaw();
@@ -405,8 +408,8 @@ public class DriveSubsystem extends EntechSubsystem {
 
             Translation2d initialTranslation = new Translation2d(Units.inchesToMeters(FIELD_LENGTH_INCHES / 2),
                     Units.inchesToMeters(FIELD_WIDTH_INCHES / 2)); // mid field
-            Rotation2d initialRotation = Rotation2d.fromDegrees(0);
-            m_gyro.setAngleAdjustment(180);
+            Rotation2d initialRotation = Rotation2d.fromDegrees(180);
+            m_gyro.setAngleAdjustment(0);
             Pose2d initialPose = new Pose2d(initialTranslation, initialRotation);
             resetOdometry(initialPose);
         }
