3356af2b25703136d274b16854f710beb673eb3d
WhyDoesGodDoThis <101780103+WhyDoesGodDoThis@users.noreply.github.com>
2023-11-27 18:52:02 -0500

    ready for mere

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -24,13 +24,6 @@ public final class Main {
      * If you change your main robot class, change the parameter type.
      */
     public static void main(String... args) {
-        if (false) {
-            try {
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-
-            }
-        }
         RobotBase.startRobot(Robot::new);
     }
 }
