a8ce8b6c6eb77bb53f1bd07917603172598e8014
John-Molfetner <jtmolfenter07@gmail.com>
2024-02-17 13:36:24 -0500

    removed variables

--- src/main/java/frc/robot/subsystems/PivotOutput.java
+++ src/main/java/frc/robot/subsystems/PivotOutput.java
@@ -5,8 +5,6 @@ import entech.subsystems.SubsystemOutput;
 public class PivotOutput implements SubsystemOutput{
 
     public boolean moving = false;
-    public boolean isAtLowerLimit = false;
-    public boolean isAtUpperLimit = false;
     public boolean brakeModeEnabled = false;
     public double currentPosition = 0.0;
 
