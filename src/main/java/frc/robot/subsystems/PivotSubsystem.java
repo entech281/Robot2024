5f8a0e3a92180063e155f5fce7cfc4313614646d
John-Molfetner <jtmolfenter07@gmail.com>
2024-02-17 13:36:24 -0500

    removed variables

--- src/main/java/frc/robot/subsystems/PivotSubsystem.java
+++ src/main/java/frc/robot/subsystems/PivotSubsystem.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkPIDController;
 
