77b9a7b8db572a518df99a7c09b864564a1b9fd8
eshi <eric.shi.sc@gmail.com>
2024-02-24 17:41:32 -0500

    all tests pass

--- src/test/java/frc/robot/processors/TestNoteAlignProcessor.java
+++ src/test/java/frc/robot/processors/TestNoteAlignProcessor.java
@@ -22,38 +22,55 @@ public class TestNoteAlignProcessor {
   @Test
   void noNote() {
     DriveInput di = new DriveInput();
-    di.setXSpeed(0.2);
+    double initialSpeed = 0.2;
+    di.setXSpeed(initialSpeed);
     NoteDetectorOutput no = new NoteDetectorOutput();
+    no.setMidpoint(Optional.empty());
     NoteAlignProcessor ni = new NoteAlignProcessor();
-    //assertEquals(0.2, ni.keepAligned(di, no).getXSpeed());
+    assertEquals(initialSpeed, ni.keepAligned(di, no).getXSpeed());
   }
 
   @Test
   void centered() {
     DriveInput di = new DriveInput();
-    Pose2d lop = new Pose2d(new Translation2d(0, 0), new Rotation2d(60));
+    double angle = Math.PI/3;
+    di.setXSpeed(Math.cos(angle));
+    di.setYSpeed(-Math.sin(angle));
+    Pose2d lop = new Pose2d(new Translation2d(0, 0), new Rotation2d(angle));
     di.setLatestOdometryPose(lop);
     NoteDetectorOutput no = new NoteDetectorOutput();
     no.setMidpoint(Optional.of(new Point(RobotConstants.Vision.Cameras.COLOR_RESOLUTION[0]/2, RobotConstants.Vision.Cameras.COLOR_RESOLUTION[1]/2)));
-    no.setYaw(0);
     NoteAlignProcessor ni = new NoteAlignProcessor();
-    //assertEquals(0.5, ni.keepAligned(di, no).getXSpeed(), 0.01);
+    assertEquals(Math.cos(angle), ni.keepAligned(di, no).getXSpeed(), 0.01);
   }
 
   @Test
   void offsetLeft() {
     DriveInput di = new DriveInput();
+    double angle = Math.PI/2;
+    di.setXSpeed(1);
+    di.setYSpeed(0);
+    Pose2d lop = new Pose2d(new Translation2d(0, 0), new Rotation2d(angle));
+    di.setLatestOdometryPose(lop);
     NoteDetectorOutput no = new NoteDetectorOutput();
+    double pixelsOffset30Degrees = 56.1467;
+    no.setMidpoint(Optional.of(new Point(pixelsOffset30Degrees, RobotConstants.Vision.Cameras.COLOR_RESOLUTION[1]/2)));
     NoteAlignProcessor ni = new NoteAlignProcessor();
-    //assertEquals(true, ni.keepAligned(di, no).getRotation()<0);
+    assertEquals(-Math.sin(angle), ni.keepAligned(di, no).getYSpeed(), 0.01);
   }  
 
   @Test
   void offsetRight() {
     DriveInput di = new DriveInput();
+    double angle = Math.PI/3;
+    di.setXSpeed(1);
+    di.setYSpeed(0);
+    Pose2d lop = new Pose2d(new Translation2d(0, 0), new Rotation2d(angle));
+    di.setLatestOdometryPose(lop);
     NoteDetectorOutput no = new NoteDetectorOutput();
+    no.setMidpoint(Optional.of(new Point(RobotConstants.Vision.Cameras.COLOR_RESOLUTION[0]/2, RobotConstants.Vision.Cameras.COLOR_RESOLUTION[1]/2)));
     NoteAlignProcessor ni = new NoteAlignProcessor();
-    //assertEquals(true, ni.keepAligned(di, no).getRotation()>0);
+    assertEquals(-Math.sin(angle), ni.keepAligned(di, no).getYSpeed(), 0.01);
   }
 
 }
