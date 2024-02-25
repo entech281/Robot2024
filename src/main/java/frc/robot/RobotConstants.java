d72ccefe626763b99cbce4b932d5df21cf50b8d8
John-Molfetner <jtmolfenter07@gmail.com>
2024-02-24 19:49:25 -0500

    toggle color

--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -114,6 +114,7 @@ public final class RobotConstants {
   public static interface LED {
     public static final int PORT = 1;
     public static final int NUM_LEDS = 42;
+    public static final double BLINK_INTERVAL = 0.25;
   }
 
 
