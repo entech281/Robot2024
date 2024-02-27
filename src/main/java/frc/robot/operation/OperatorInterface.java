ffac1253e81b88a0211d29a2e801524271d8709d
eshi <eric.shi.sc@gmail.com>
2024-02-26 20:05:17 -0500

    incomplete oi

--- src/main/java/frc/robot/operation/OperatorInterface.java
+++ src/main/java/frc/robot/operation/OperatorInterface.java
@@ -5,9 +5,11 @@ import entech.util.EntechJoystick;
 import frc.robot.CommandFactory;
 import frc.robot.RobotConstants;
 import frc.robot.SubsystemManager;
+import frc.robot.commands.DoNothing;
 import frc.robot.commands.DriveCommand;
 import frc.robot.commands.GyroReset;
 import frc.robot.commands.TwistCommand;
+import frc.robot.commands.IntakeCommand;
 import frc.robot.io.DebugInput;
 import frc.robot.io.DebugInputSupplier;
 import frc.robot.io.DriveInputSupplier;
@@ -37,11 +39,26 @@ public class OperatorInterface
   }
 
   public void create() {
+    driverBindings();
+    operatorBindings();
+  }
+
+  public void driverBindings() {
     driveJoystick.whilePressed(1, new TwistCommand());
     driveJoystick.whenPressed(11, new GyroReset(subsystemManager.getNavXSubsystem(), odometry));
 
     subsystemManager.getDriveSubsystem()
         .setDefaultCommand(new DriveCommand(subsystemManager.getDriveSubsystem(), this));
+    driveJoystick.whilePressed(2, new IntakeCommand(subsystemManager.getIntakeSubsystem(), subsystemManager.getTransferSubsystem()));
+    driveJoystick.whilePressed(3, new DoNothing()); // align to speaker or amp depending on an operator switch
+  }
+
+  public void operatorBindings() {
+    operatorPanel.button(1).whileTrue(new DoNothing()); // shoot speaker
+    operatorPanel.button(2).whileTrue(new DoNothing()); // shoot amp
+    operatorPanel.whileSwitch(3, new IntakeCommand(subsystemManager.getIntakeSubsystem(), subsystemManager.getTransferSubsystem()), new DoNothing()); // run intake and transfer backwards and eject note
+    operatorPanel.button(4).whileTrue(new DoNothing()); // advance to next stage of climb
+    operatorPanel.button(5).onTrue(new DoNothing()); // revert to last state of climb
   }
 
   /*
