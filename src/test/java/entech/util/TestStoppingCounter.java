935173eb7af31957c6470914338aeac9f2ee2f59
John-Molfetner <jtmolfenter07@gmail.com>
2024-02-29 12:03:09 -0500

    fixed tests

--- src/test/java/entech/util/TestStoppingCounter.java
+++ src/test/java/entech/util/TestStoppingCounter.java
@@ -7,21 +7,21 @@ public class TestStoppingCounter {
 
   @Test
   void testStopperStopsAllSuccess() {
-    final double NUM_COUNTS = 4;
+    final double NUM_SECONDS = 0.08;
 
-    StoppingCounter sc = new StoppingCounter("quicketest", NUM_COUNTS);
+    StoppingCounter sc = new StoppingCounter("quicketest", NUM_SECONDS);
     assertFalse(sc.isFinished(true));
     assertFalse(sc.isFinished(true));
     assertFalse(sc.isFinished(true));
     assertFalse(sc.isFinished(true));
-    assertFalse(sc.isFinished(true));
+    assertTrue(sc.isFinished(true));
   }
 
   @Test
   void testStopperResets() {
-    final double NUM_COUNTS = 4;
+    final double NUM_SECONDS = 0.08;
 
-    StoppingCounter sc = new StoppingCounter("quicketest", NUM_COUNTS);
+    StoppingCounter sc = new StoppingCounter("quicketest", NUM_SECONDS);
     assertFalse(sc.isFinished(true));
     assertFalse(sc.isFinished(true));
     assertFalse(sc.isFinished(true));
@@ -30,7 +30,7 @@ public class TestStoppingCounter {
     assertFalse(sc.isFinished(true));
     assertFalse(sc.isFinished(true));
     assertFalse(sc.isFinished(true));
-    assertFalse(sc.isFinished(true));
+    assertTrue(sc.isFinished(true));
   }
 
 }
