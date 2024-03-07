18dc08e3ea690dfe2de5356edf70223b079217ae
281developer <dave.cowden@gmail.com>
2024-03-07 14:37:01 -0500

    A try at a backup script for deployment

---
+++ deploy_robot.bat
@@ -0,0 +1,15 @@
+if [%1]==[] goto usage
+git remote set-url origin git@10.2.81.10:entech281/Robot2024.git
+git fetch --all
+git pull
+git checkout  %1
+.\gradlew build
+.\gradlew deploy
+
+goto :eof
+:usage
+@echo
+@echo Deploys the selected branch from repository to the robot.
+@echo Usage: %0 ^<BranchName^>
+exit /B 1
+
