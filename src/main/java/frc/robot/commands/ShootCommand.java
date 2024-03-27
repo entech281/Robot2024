34fbcaf84711f72e3c5b31fa0970a803150bdc0a
John-Molfetner <jtmolfenter07@gmail.com>
2024-03-27 19:49:31 -0400

    Sadra broke the robot

--- src/main/java/frc/robot/commands/ShootCommand.java
+++ src/main/java/frc/robot/commands/ShootCommand.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import entech.commands.EntechCommand;
 import entech.util.StoppingCounter;
@@ -40,6 +41,31 @@ public class ShootCommand extends EntechCommand {
     this.speakerSwitch = speakerSwitch;
   }
 
+  // angleA is the angle that the angle of the arm when it is flat
+  // sideB is the length from the pivot point to the shooter
+  // targetPos is the virtual position that the arm should be aiming at
+  // originPos is the location of the pivot in the field
+  private static final double aimToPoint(double angleA, double sideB, double targetPos[],
+      double originPos[]) {
+    double sideA = Math.sqrt((Math.pow((targetPos[1] - originPos[1]), 2)
+        + (Math.pow((targetPos[0] - originPos[0]), 2))));
+
+    double angleB = Math.toDegrees(Math.sin(sideB * Math.sin(Math.toRadians(angleA)) / sideA));
+
+    double angleC = 180 - angleB - angleA;
+
+    double sideD = Math.sqrt(
+        Math.pow((originPos[0] - targetPos[0]), 2) + Math.pow((originPos[2] - targetPos[2]), 2));
+
+    double sideE = originPos[1] - targetPos[1];
+
+    double angleE = Math.toDegrees(Math.acos(
+        (Math.pow(sideA, 2) + Math.pow(sideD, 2) - Math.pow(sideE, 2)) / (2 * sideA * sideD)));
+
+    double angleTheta = 180 - (angleC + angleE);
+    return angleTheta;
+  }
+
   @Override
   public void initialize() {
     cancelCounter.reset();
@@ -49,22 +75,27 @@ public class ShootCommand extends EntechCommand {
       noNote = false;
       sInput.setActivate(true);
       pInput.setActivate(true);
-
-      if (ampSwitch.getAsBoolean()) {
-        pInput.setRequestedPosition(RobotConstants.PIVOT.SHOOT_AMP_POSITION_DEG);
-        sInput.setSpeed(RobotConstants.PID.SHOOTER.AMP_SPEED);
-      } else if (speakerSwitch.getAsBoolean()) {
-        pInput.setRequestedPosition(RobotConstants.PIVOT.SPEAKER_PODIUM_SCORING);
-        sInput.setSpeed(RobotConstants.PID.SHOOTER.PODIUM_SPEED);
-      } else {
-        pInput.setRequestedPosition(RobotConstants.PIVOT.SPEAKER_SUBWOOFER_SCORING);
-        sInput.setSpeed(RobotConstants.PID.SHOOTER.SUBWOOFER_SPEED);
-      }
-      sSubsystem.updateInputs(sInput);
-      pSubsystem.updateInputs(pInput);
     } else {
       noNote = true;
     }
+
+    pInput.setRequestedPosition(aimToPoint(57.0, Units.inchesToMeters(15),
+        new double[] {0.0, Units.inchesToMeters(80), 0.0},
+        new double[] {RobotIO.getInstance().getOdometryPose().getX(), Units.inchesToMeters(18),
+            RobotIO.getInstance().getOdometryPose().getY()}));
+
+    if (ampSwitch.getAsBoolean()) {
+      // pInput.setRequestedPosition(RobotConstants.PIVOT.SHOOT_AMP_POSITION_DEG);
+      sInput.setSpeed(RobotConstants.PID.SHOOTER.AMP_SPEED);
+    } else if (speakerSwitch.getAsBoolean()) {
+      // pInput.setRequestedPosition(RobotConstants.PIVOT.SPEAKER_PODIUM_SCORING);
+      sInput.setSpeed(RobotConstants.PID.SHOOTER.PODIUM_SPEED);
+    } else {
+      // pInput.setRequestedPosition(RobotConstants.PIVOT.SPEAKER_SUBWOOFER_SCORING);
+      sInput.setSpeed(RobotConstants.PID.SHOOTER.SUBWOOFER_SPEED);
+    }
+    sSubsystem.updateInputs(sInput);
+    pSubsystem.updateInputs(pInput);
   }
 
   @Override
