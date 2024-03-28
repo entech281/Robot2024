0073eb6a27720d49353b79452b9a676b722826cf
Sadra Saharkhiz <sarkh200@gmail.com>
2024-03-28 16:34:28 -0400

    added AimCalculator.java

---
+++ src/main/java/entech/util/AimCalculator.java
@@ -0,0 +1,43 @@
+package entech.util;
+
+import java.lang.Math;
+import edu.wpi.first.math.util.Units;
+
+public class AimCalculator {
+	private static final double pivotHeightInches = 18;
+	private static final double armLenInches = 15;
+	private static final double armAngleDegrees = 57;
+
+	/**
+	 * 
+	 * @param distanceToTargetMeters
+	 * @return Angle the arm needs to be at in order to hit a specific target with the note
+	 */
+	public static double getAngleDegreesFromDistance(double distanceToTargetMeters, double targetHeightInches) {
+		double distanceToTargetInches = Units.metersToInches(distanceToTargetMeters);
+		return Units.inchesToMeters(getAngleDegreesFromDistance(distanceToTargetInches, targetHeightInches, pivotHeightInches, armLenInches, armAngleDegrees));
+	}
+
+	private static final double getAngleDegreesFromDistance(double distanceToTarget, double heightOfTarget,
+			double heightOfPivot, double lengthOfArm, double angleOfShooter) {
+
+		double sideA = Math.sqrt(
+				Math.pow(distanceToTarget, 2) + Math.pow((heightOfTarget - heightOfPivot), 2));
+
+		double angleB = Math.toDegrees(
+				Math.sin(lengthOfArm * Math.sin(Math.toRadians(angleOfShooter)) / sideA));
+
+		double angleC = 180 - angleB - angleOfShooter;
+
+		double sideD = distanceToTarget;
+
+		double sideE = heightOfTarget - heightOfPivot;
+
+		double angleE = Math
+				.toDegrees(Math.acos((Math.pow(sideA, 2) + Math.pow(sideD, 2) - Math.pow(sideE, 2))
+						/ (2 * sideA * sideD)));
+
+		double angleTheta = 180 - (angleC + angleE);
+		return angleTheta;
+	}
+}
